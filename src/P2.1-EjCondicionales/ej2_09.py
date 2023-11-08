4be2e0a538124eb5ec7d4cb6a5e0d93059a047fb
Andresinii <anmaca2004@gmail.com>
2023-11-08 13:39:40 +0100

    EJERCICIOS 2.1 CONDICIONALES FINALIZADOS

--- src/P2.1-EjCondicionales/ej2_09.py
+++ src/P2.1-EjCondicionales/ej2_09.py
@@ -1,2 +1,18 @@
+def entrada(edad):
+    if edad < 4:
+        print("Usted entra gratis.")
+    elif edad >= 4 and edad <18:
+        print("El precio de la entrada es de 5€.")
+    elif edad >= 18:
+        print("El precio de la entrada es de 10€.")
+
+
+def main():
+    edad = int(input("Indique su edad: "))
+    while edad < 0 or edad > 100:
+        edad = int(input("ERROR! Indique una edad válida: "))
+
+    entrada(edad)
+
 if __name__ == "__main__":
     main()
--- src/P2.1-EjCondicionales/ej2_10.py
+++ src/P2.1-EjCondicionales/ej2_10.py
@@ -1,5 +1,29 @@
+def ingredientes(opcion):
+    if opcion == "si":
+        eleccion = input("Todas las pizzas llevan tomate y mozzarella. Además, puede añadir pimiento o tofu. ¡Escoja uno!: ")
+        tipo = "vegetariana"
+        eleccion = eleccion.lower()
+        while eleccion != "pimiento" and eleccion != "tofu":
+            eleccion = input("ERROR! Elija un ingrediente: ")
+    else:
+        eleccion = input("Todas las pizzas llevan tomate y mozzarella. Además, puede añadir pepperoni, jamón o salmón. ¡Escoja uno!: ")
+        tipo = "normal"
+        eleccion = eleccion.lower()
+        while eleccion != "pepperoni" and eleccion != "jamón" and eleccion != "jamon" and eleccion != "salmón" and eleccion != "salmon":
+            eleccion = input("ERROR! Elija un ingrediente: ")
+
+    return eleccion, tipo
+
 def main():
-    vegetariana = input("¿Quiere usted una pizza vegetariana? ")
+    opcion = input("¿Quiere usted una pizza vegetariana? (SI/NO) ")
+    opcion = opcion.lower()
+    while opcion != "si" and opcion != "no":
+        opcion = input("ERROR! Responda con SI o NO:  ")
+    
+    eleccion, tipo = ingredientes(opcion)
+
+    print(f"La pizza escogida es {tipo} y lleva tomate, mozzarella y {eleccion}.")
+
 
 if __name__ == "__main__":
     main()
